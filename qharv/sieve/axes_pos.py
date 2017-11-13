d7dc8971a69d0ccc64800cd9d4ab3e19a97784bd
Paul Young <yyang173@illinois.edu>
2017-11-12 22:08:11 -0600

    calculate r_inscribed

--- qharv/sieve/axes_pos.py
+++ qharv/sieve/axes_pos.py
@@ -4,6 +4,33 @@
 import numpy as np
 import pandas as pd
 
+def volume(axes):
+  """ volume of a simulation cell
+  Args:
+    axes (np.array): lattice vectors in row-major
+  Returns:
+    float: volume of cell
+  """
+  return np.dot(axes[0],np.cross(axes[1],axes[2]))
+# end def volume
+
+def rins(axes):
+  """ radius of the inscribed sphere inside the given cell
+  Args:
+    axes (np.array): lattice vectors in row-major
+  Returns:
+    float: radius of the inscribed sphere
+  """
+  a01 = np.cross(axes[0],axes[1])
+  a12 = np.cross(axes[1],axes[2])
+  a20 = np.cross(axes[2],axes[0])
+  face_areas = [np.linalg.norm(x) for x in [a01,a12,a20]]
+
+  # 2*rins is the height from face
+  rins = volume(axes)/2./max(face_areas)
+  return rins
+# end def rins
+
 def properties_from_axes_pos(axes,pos):
   """ calculate properties from axes,pos alone
    essentially the simplified/customized version of:
@@ -17,15 +44,15 @@ def properties_from_axes_pos(axes,pos):
 
   # canonical properties
   natom  = len(pos)
-  volume = np.dot(axes[0],np.cross(axes[1],axes[2]))
-  vol_pp = volume/natom # volume per particle
+  vol    = volume(axes)
+  vol_pp = vol/natom # volume per particle
   rs     = (3.*vol_pp/(4*np.pi))**(1./3)
   inv_axes = np.linalg.inv(axes)
   upos   = np.dot(pos,inv_axes) # fractional coordinates
 
   entry = {}
-  name_list = ['natom','volume','vol_pp','rs','inv_axes','upos']
-  val_list  = [natom,volume,vol_pp,rs,inv_axes,upos]
+  name_list = ['natom','volume','vol_pp','rs','rins','inv_axes','upos']
+  val_list  = [natom,vol,vol_pp,rs,rins(axes),inv_axes,upos]
   for name,val in zip(name_list,val_list):
     entry[name] = val
   # end for
