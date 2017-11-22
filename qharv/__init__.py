a1ee5d22ce682b6c317db6618af36927840d26c4
Paul Young <yyang173@illinois.edu>
2017-11-22 11:37:50 -0600

    more text fixes

--- qharv/__init__.py
+++ qharv/__init__.py
@@ -1,5 +1,5 @@
 __version__ = '0.0'
-__all__ = ['seed','inspect','cross','trans','reel'] # TODO: refine
+__all__ = ['seed','inspect','cross','trans','reel','sieve'] # TODO: refine
 # sown the seed, inspect the crop, 
 #  crossbreed to improve, transplant to adapt,
 #   reel them in, sieve for good, and refine for the best.
