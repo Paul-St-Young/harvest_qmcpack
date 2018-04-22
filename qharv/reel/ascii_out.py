31e3f7c67f73cb7f8d21310832a1172a779cd1e7
Paul Young <yyang173@illinois.edu>
2018-04-22 12:21:42 -0500

    fix doc str

--- qharv/reel/ascii_out.py
+++ qharv/reel/ascii_out.py
@@ -1,7 +1,7 @@
 # Author: Yubo "Paul" Yang
 # Email: yubo.paul.yang@gmail.com
 # Routines to parse ASCII output. Mostly built around mmap's API.
-
+#  The central object is mmap.mmap, which is usually named "mm".
 from mmap import mmap
 import pandas as pd
 
@@ -14,15 +14,13 @@ def read(fname):
   Return:
     mmap.mmap: memory map to file
   """
-  with open(fname,'r+') as f:
-    mm = mmap(f.fileno(),0)
-  # end with
+  with open(fname, 'r+') as f:
+    mm = mmap(f.fileno(), 0)
   return mm
-# end def read
 
 
-def name_sep_val(mm,name,sep='=',dtype=float,pos=-1):
-  """ read key-value pair
+def name_sep_val(mm, name, sep='=', dtype=float, pos=-1):
+  """ read key-value pair such as "name = value"
   e.g. 
   name_sep_val(mm, 'a'): 'a = 2.4'
   name_sep_val(mm, 'volume', pos=-2): 'volume = 100.0 bohr^3'
@@ -31,9 +29,13 @@ def name_sep_val(mm,name,sep='=',dtype=float,pos=-1):
   name_sep_val(mm, 'natom', dtype=int): 'new:name'
 
   Args:
-    fname (str): filename
+    mm (mmap.mmap): memory map
+    name (str): name of variable; used to find value line
+    sep (str, optional): separator, default '='
+    dtype (type, optional): variable data type, default float
+    pos (int, optiona): position of value in line, default last (-1)
   Return:
-    mmap.mmap: memory map to file
+    dtype: value of requested variable
   """
   idx = mm.find(name)
   if idx == -1:
@@ -49,9 +51,18 @@ def name_sep_val(mm,name,sep='=',dtype=float,pos=-1):
 # end def name_sep_val
 
 
-def all_lines_with_tag(mm,tag,nline_max=1024*1024):
+def all_lines_with_tag(mm, tag, nline_max=1024*1024):
   """ return a list of memory indices pointing to the start of tag
-   the search is conducted starting from the current location of mm. """
+   the search is conducted starting from the current location of mm.
+
+  Args:
+    mm (mmap.mmap): memory map to file
+    tag (str): tag to identify lines to look for
+    nline_max (int, optional): maximum number of lines to look for
+     , default is 2^20. Error will be raised if max is too low
+  Return:
+    list: a list of memory locations of all found tags
+  """
   all_idx = []
   for iline in range(nline_max):
     idx = mm.find(tag)
@@ -68,45 +79,105 @@ def all_lines_with_tag(mm,tag,nline_max=1024*1024):
     raise RuntimeError('may need to increase nline_max')
   # end if
   return all_idx
-# end def all_lines_with_tag
+# end def
 
 
-def all_lines_at_idx(mm,idx_list):
+def all_lines_at_idx(mm, idx_list):
+  """ return a list of lines given a list of memory locations
+  follow up on all_lines_with_tag
+  e.g. all_lines_at_idx(mm, all_lines_with_tag(mm, 'Atom') )
+  reads '''
+  Atom 0 0 0 0
+  Atom 1 1 1 1
+  Atom 2 2 2 2
+  '''
+
+  Args:
+    mm (mmap.mmap): memory map to file
+    idx_list (list): a list of memory locations (int)
+  Return:
+    list: a list of strings, each being the line at idx
+  """
   lines = []
   for idx in idx_list:
     mm.seek(idx)
-    lines.append( mm.readline() )
+    line = mm.readline()
+    lines.append(line)
   # end for
   return lines
 # end def
 
 
-def locate_block(mm,header,trailer,skip_header=True,skip_trailer=True):
-  begin_idx = mm.find(header.encode())
+def locate_block(mm, header, trailer
+  , skip_header=True, skip_trailer=True):
+  """ find the memory locations bounding a block of text
+  in between header and trailer; header and trailer are
+  not included by default
+  e.g. see block_text
+
+  Args:
+    mm (mmap.mmap): memory map to text file
+    header  (str): string indicating the beginning of block
+    trailer (str): string indicating the end of block
+    skip_head (bool, optional): skip header, default is True
+    skip_trailer (bool, optional): skip trailer, default is True
+  Return:
+    tuple: (begin_idx, end_idx), memory span of text block
+  """
+  begin_idx = mm.find( header.encode() )
   if skip_header:
     mm.seek(begin_idx)
     mm.readline()
     begin_idx = mm.tell()
   # end if
-  end_idx   = mm.find(trailer.encode())
+  end_idx = mm.find( trailer.encode() )
   if not skip_trailer:
     mm.seek(end_idx)
     mm.readline()
     end_idx = mm.tell()
   # end if
-  return begin_idx,end_idx
+  return begin_idx, end_idx
 # end def locate_block
 
 
-def block_text(mm,header,trailer,**kwargs):
+def block_text(mm, header, trailer, **kwargs):
+  """ find a block of text in between header and trailer
+  header and trailer are not included by default
+
+  e.g. given text in mm
+  '''
+  begin important data
+  1 2 3
+  4 5 6
+  7 8 9
+  end important data
+  '''
+
+  mm.block_text(mm, 'begin', 'end') returns
+  '''1 2 3
+  4 5 6
+  7 8 9
+  '''
+
+  Args:
+    mm (mmap.mmap): memory map to text file
+    header  (str): string indicating the beginning of block
+    trailer (str): string indicating the end of block
+  """
   bidx,eidx = locate_block(mm,header,trailer,**kwargs)
   return mm[bidx:eidx]
 # end def block_text
 
 
-def lr_mark(line,lmark,rmark):
+def lr_mark(line, lmark, rmark):
   """ read a string segment from line, which is enclosed between l&rmark
    e.g. extract the contents in parenteses
+  Args:
+    line (str): text line
+    lmark (str): left marker, e.g. '('
+    rmark (str): right marker, e.g. ')'
+  Return:
+    str: text in between left and right markers
   """
   lidx = line.find(lmark)
   assert lidx != -1
