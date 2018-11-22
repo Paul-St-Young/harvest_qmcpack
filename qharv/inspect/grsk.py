935458ee990e676c16a8af3f61e493917f4d1bf0
Paul Young <yyang173@illinois.edu>
2018-11-22 10:57:57 -0600

    starting grsk

---
+++ qharv/inspect/grsk.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def ft_iso3d(myk, myr, frm):
+  fkm = [np.trapz(myr*np.sin(k*myr)/k*frm, myr) for k in myk]
+  return 4*np.pi*np.array(fkm)
+
+def gr2sk(myk, myr, grm, rho):
+  skm = 1+rho*ft_iso3d(myk, myr, grm-1)
+  return skm
+
+def sk2gr(myr, myk, skm, rho):
+  grm = 1+ft_iso3d(myr, myk, skm-1)/rho/(2*np.pi)**3
+  return grm
