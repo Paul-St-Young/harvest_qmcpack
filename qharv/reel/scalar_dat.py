d037660952e017ea705775c87ba9d7420ab9054f
Paul Young <yyang173@illinois.edu>
2019-03-21 22:02:05 -0500

    formating

--- qharv/reel/scalar_dat.py
+++ qharv/reel/scalar_dat.py
@@ -23,37 +23,29 @@ def read(dat_fname):
 
   Args:
     dat_fname (str): name of input file
-  Returns:
+  Return:
     pd.DataFrame: df containing the table of data
+  Example:
+    >>> df = read('vmc.s001.scalar.dat')
   """
-
   # check if a header line exists, if it does not, then set header=None
-  with open(dat_fname,'r') as fp:
+  with open(dat_fname, 'r') as fp:
     header = fp.readline().strip()
-  # end with
-  
-  if not header.startswith('#'): # there is no header
+  if not header.startswith('#'):  # there is no header
     # pandas's equivalent of numpy.loadtxt
-    df = pd.read_csv(dat_fname,sep='\s+',header=None)
-  else: # do some extra parsing of the header
-    df = pd.read_csv(dat_fname,sep='\s+')
-
+    df = pd.read_csv(dat_fname, sep='\s+', header=None)
+  else:  # do some extra parsing of the header
+    df = pd.read_csv(dat_fname, sep='\s+')
     # remove first column name '#'
     columns = df.columns
-    df.drop(columns[-1],axis=1,inplace=True)
+    df.drop(columns[-1], axis=1, inplace=True)
     df.columns = columns[1:]
-
     # calculate local energy variance if possible
     if ('LocalEnergy' in columns) and ('LocalEnergy_sq' in columns):
       df['Variance'] = df['LocalEnergy_sq']-df['LocalEnergy']**2.
-    # end if
-  # end if
-
   # column labels should be strings
-  df.columns = map(str,df.columns)
-
+  df.columns = map(str, df.columns)
   return df
-# end def parse
 
 def read_to_list(dat_fname):
   """ read scalar.dat file into a list of pandas DataFrames
@@ -65,6 +57,9 @@ def read_to_list(dat_fname):
     dat_fname (str): name of input file
   Return:
     list: list of df(s) containing the table(s) of data
+  Example:
+    >>> dfl = read_to_list('gctas.dat')
+    >>> df = pd.concat(dfl).reset_index(drop=True)
   """
   # first separate out the header lines and parse them
   from StringIO import StringIO
@@ -97,7 +92,8 @@ def write(dat_fname, df, header_pad='# ', **kwargs):
   Args:
     dat_fname (str): output data file name
     df (pd.DataFrame): data
-    header_pad (str, optional): pad beginning of header with comment string, default '# '
+    header_pad (str, optional): pad beginning of header with comment
+     string, default '# '
   """
   default_kws = {
     'index': False,
@@ -113,22 +109,21 @@ def write(dat_fname, df, header_pad='# ', **kwargs):
 def corr(trace):
   """ calculate the autocorrelation of a trace of scalar data
 
-  correlation time is defined as the integral of the auto-correlation function from t=0 to when the function first reaches 0.
+  correlation time is defined as the integral of the auto-correlation
+   function from t=0 to when the function first reaches 0.
 
   Args:
     trace (list): should be a 1D iterable array of floating point numbers
-  Returns:
+  Return:
     float: correlation_time, the autocorrelation time of this trace of scalars
   """
- 
-  mu     = np.mean(trace)
-  stddev = np.std(trace,ddof=1)
-  if np.isclose(stddev,0): # easy case
-    return np.inf # infinite correlation for constant trace
-  # end if
 
+  mu     = np.mean(trace)
+  stddev = np.std(trace, ddof=1)
+  if np.isclose(stddev, 0):  # easy case
+    return np.inf  # infinite correlation for constant trace
   correlation_time = 0.
-  for k in range(1,len(trace)):
+  for k in range(1, len(trace)):
     # calculate auto_correlation
     auto_correlation = 0.0
     num = len(trace)-k
@@ -138,39 +133,32 @@ def corr(trace):
       correlation_time += auto_correlation
     else:
       break
-    # end if
-  # end for k
- 
   correlation_time = 1.0 + 2.0*correlation_time
   return correlation_time
-# end def corr
-def error(trace,kappa=None):
+
+def error(trace, kappa=None):
   """ calculate the error of a trace of scalar data
 
   Args:
     trace (list): should be a 1D iterable array of floating point numbers
-    kappa (float,optional): auto-correlation time, default is to re-calculate 
-  Returns:
+    kappa (float,optional): auto-correlation time, default is to re-calculate
+  Return:
     float: stderr, the error of the mean of this trace of scalars
   """
-  stddev = np.std(trace,ddof=1)
-  if np.isclose(stddev,0): # easy case
-    return 0.0 # no error for constant trace
-  # end if
-
-  if kappa is None: # no call to corr 
+  stddev = np.std(trace, ddof=1)
+  if np.isclose(stddev, 0):  # easy case
+    return 0.0  # no error for constant trace
+  if kappa is None:  # no call to corr
     kappa = corr(trace)
-  # end if
   neffective = np.sqrt(len(trace)/kappa)
   err = stddev/neffective
   return err
-# end def error
 
-def single_column(df,column,nequil):
+def single_column(df, column, nequil):
   """ calculate mean and error of a column
 
-    nequil blocks of data are thrown out; autocorrelation time is taken into 
-  account when calculating error. The equilibrated data is assumed to have 
+    nequil blocks of data are thrown out; autocorrelation time is taken into
+  account when calculating error. The equilibrated data is assumed to have
   Gaussian distribution. Error is calculated for one standard deviation
   (1-sigma error).
 
@@ -178,7 +166,7 @@ def single_column(df,column,nequil):
     df (pd.DataFrame): table of data (e.g. from scalar_dat.parse)
     column (str): name of column
     nequil (int): number of equilibration blocks
-  Returns:
+  Return:
     (float,float,float): (ymean,yerr,ycorr), where ymean is the mean of column
      , yerr is the 1-sigma error of column, and ycorr is the autocorrelation
   """
@@ -187,7 +175,5 @@ def single_column(df,column,nequil):
 
   ymean = np.mean(myy)
   ycorr = corr(myy)
-  yerr  = error(myy,ycorr)
-
-  return ymean,yerr,ycorr
-# end def
+  yerr  = error(myy, ycorr)
+  return ymean, yerr, ycorr
