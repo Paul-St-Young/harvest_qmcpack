749e1ee38c7747f0db1279798579c826ac148b83
Paul Young <yyang173@illinois.edu>
2019-05-22 17:24:29 -0500

    ase_tile

--- qharv/inspect/axes_elem_pos.py
+++ qharv/inspect/axes_elem_pos.py
@@ -4,7 +4,6 @@
 #  extend axes_pos to handle more than 1 species of atoms
 import numpy as np
 
-
 def ase_read(floc):
   """ use atomic simulation environment (ase) package to read file
   can read any format ase.read supports
@@ -26,3 +25,24 @@ def ase_read(floc):
     'pos': pos
   }
   return data
+
+def ase_tile(axes, elem, pos, tmat):
+  """ use ase to tile supercell
+
+  Args:
+    axes (np.array): lattice vectors in row-major
+    elem (np.array): chemical symbols
+    pos (np.array): atomic positions
+    tmat (np.array): tiling matrix (a.k.a. supercell matrix)
+  Return:
+    (np.array, np.array, np.array): supercell ('axes', 'elem', 'pos')
+  """
+  from ase import Atoms
+  from ase.build import make_supercell
+  pbc = [True]*3  # assume PBC if tiling
+  s0 = Atoms(''.join(elem), cell=axes, positions=pos, pbc=pbc)
+  s1 = make_supercell(s0, tmat)
+  axes1 = s1.get_cell()
+  elem1 = s1.get_chemical_symbols()
+  pos1 = s1.get_positions()
+  return axes1, elem1, pos1
