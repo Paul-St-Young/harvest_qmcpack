fix weighted twist average error propagation

qharv/sieve/scalar_h5.py
@@ -56,7 +56,7 @@ def twist_average_h5(fh5, weights=None, **suffix_kwargs):
   # twist average with weights
   try:
     ym = np.sum(weights[:, np.newaxis, np.newaxis]*yma, axis=0)/wtot
-    ye = np.sum(weights[:, np.newaxis, np.newaxis]*yea**2, axis=0)**0.5/wtot
+    ye = np.sum(weights[:, np.newaxis, np.newaxis]**2*yea**2, axis=0)**0.5/wtot
   except:
     ym = np.sum(weights[:, np.newaxis]*yma, axis=0)/wtot
     ye = np.sum(weights[:, np.newaxis]**2*yea**2, axis=0)**0.5/wtot