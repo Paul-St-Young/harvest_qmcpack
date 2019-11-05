3681f44a6c4f650c2239b1813ca30fb6298f9467
Paul Young <yyang173@illinois.edu>
2019-11-05 17:51:24 -0600

    deal with orthorhombic box

--- qharv/inspect/box_pos.py
+++ qharv/inspect/box_pos.py
@@ -1,41 +1,62 @@
 # Author: Yubo "Paul" Yang
 # Email: yubo.paul.yang@gmail.com
-# Routines to cubic structure specified by pos and lbox
+# Routines to cubic structure specified by pos and box
 import numpy as np
 
-def pos_in_box(pos, lbox):
+def pos_in_box(pos, box):
   """ Enforce periodic bounary condition (PBC) on particle positions.
-  Simulation box contains [0, L)
+  Simulation box contains [0, Lx)x[0, Ly)x[0, Lz)
 
   Args:
     pos (np.array): particle positions under open boundary conditions.
-    lbox (float): side length of cubic box
+    box (float): side lengths of box
   Return:
     np.array: particle positions under periodic bounary conditions.
   """
-  return pos % lbox
+  ndim = pos.shape[-1]
+  try:  # take care of simple case first (lx=ly=lz)
+    ndim = len(box)  # except if box is float
+    assert len(box) == ndim
+    return pos % box
+  except TypeError as err:
+    pass  # general case
+  pos1 = pos.copy()
+  for idim in range(ndim):
+    pos1[:, idim] = pos1[:, idim] % box[idim]
+  return pos1
 
-def disp_in_box(drij, lbox):
+def disp_in_box(drij, box):
   """ Enforce minimum image convention (MIC) on displacement vectors.
 
   Args:
     drij (np.array): ri - rj for all pairs of particles rij
-    lbox (float): side length of cubic box
+    box (float): side lengths of box
   Return:
     np.array: displacement vectors under MIC.
   """
-  nint = np.around(drij/lbox)
-  return drij-lbox*nint
+  ndim = drij.shape[-1]
+  try:  # take care of simple case first (lx=ly=lz)
+    ndim = len(box)  # except if box is float
+    assert len(box) == ndim
+    nint = np.around(drij/box)
+    return drij-box*nint
+  except TypeError as err:
+    pass  # general case
+  drij1 = drij.copy()
+  for idim in range(ndim):
+    nint = np.around(drij[:, :, idim]/box[idim])
+    drij[:, :, idim] -= box[idim]*nint
+  return drij1
 
-def displacement_table(pos, lbox):
+def displacement_table(pos, box):
   """ Calculate displacements ri-rj between all pairs of particles.
 
   Args:
     pos (np.array): particle positions
-    lbox (float): side length of cubic box
+    box (float): side lengths of box
   Return:
     np.array: drij, a table of displacement vectors under MIC,
      shape (natom, natom, ndim)
   """
   drij = pos[:, np.newaxis] - pos[np.newaxis]
-  return disp_in_box(drij, lbox)
+  return disp_in_box(drij, box)
