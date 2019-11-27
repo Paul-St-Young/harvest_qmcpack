5b85c9374aa5f293d20bc5b8474622c5d9c69e06
Paul Young <yyang173@illinois.edu>
2019-11-27 14:06:13 -0600

    extract categorize_columns and deprecate ta
    twist_average_mean_df is too bloated
    only the column sorting code is still useful

--- qharv/sieve/mean_df.py
+++ qharv/sieve/mean_df.py
@@ -8,11 +8,32 @@
 import numpy as np
 import pandas as pd
 
+def categorize_columns(mdf):
+  """Categorize the column names of a mean dataframe.
+
+  Args:
+    mdf (pd.DataFrame): mean dataframe
+  Return:
+    (list, list, list): (excol, mcol, ecol)
+      excol are columns of exact values with no errorbar (possibly labels)
+      mcols are mean columns
+      ecols are error columns
+  Examples:
+    >>> rcol, mcol, ecol = categorize_columns(mdf)
+    >>> xyye(df, 'Pressure', 'LocalEnergy', xerr=True)
+  """
+  cols = mdf.columns
+  mcol = [col for col in cols if col.endswith('_mean')]
+  ecol = [col for col in cols if col.endswith('_error')]
+  rcol = [col for col in cols if
+          (not col.endswith('_mean')) and (not col.endswith('_error'))]
+  return rcol, mcol, ecol
+
 def xyye(df, xname, yname, sel=None, xerr=False, yerr=True, sort=False):
   """Get x vs. y data from a mean data frame.
 
   Args:
-    df (mdf): mean dataframe
+    df (pd.DataFrame): mean dataframe
     xname (str): name of x variable
     yname (str): name of y variable
     sel (np.array, optional): boolean selector for subset, default is all
@@ -49,6 +70,19 @@ def xyye(df, xname, yname, sel=None, xerr=False, yerr=True, sort=False):
     idx = np.argsort(xm)
   return [ret[idx] for ret in rets if ret is not None]
 
+def taw(ym, ye, weights):
+  """ twist average with weights """
+  wtot = weights.sum()
+  try:
+    aym = np.dot(ym, weights)/wtot
+    aye = np.dot(ye**2, weights**2)**0.5/wtot
+  except ValueError as err:
+    if 'not aligned' not in str(err):
+      raise err
+    aym = (weights[:, np.newaxis]*ym).sum(axis=0)/wtot
+    aye = (weights[:, np.newaxis]**2*ye**2).sum(axis=0)**0.5/wtot
+  return aym, aye
+
 def dfme(df, cols, no_error=False, weight_name=None):
   """ Average scalar quantities over a set of calculations.
 
@@ -83,63 +117,3 @@ def dfme(df, cols, no_error=False, weight_name=None):
   for col, y1 in zip(mcols, ym):
     entry[col] = y1
   return pd.Series(entry)
-
-def twist_average_mean_df(df0, drop_null=False):
-  ''' Average scalar quantities over a set of calculations. The intented
-   application is to average over a uniform grid of twist calculations.
-
-  Args:
-   df0 (pd.DataFrame): a mean dataframe containing columns
-    ['path', 'fdat', '*_mean', '*_error'].
-    fdat is the scalar.dat filename. It provides 'series' and 'group' indices.
-   drop_null (bool, optional): drop rows that contain any nan. These rows
-    should be processed at a higher level (e.g. catach the RuntimeError).
-  Returns:
-    pd.DataFrame: df1, a mean dataframe containing one entry for each series.
-     df1 is structually identical to a mean dataframe of a single twist.
-  '''
-  # decide what to do with nans
-  bad_sel = df0.isnull().any(axis=1)
-  bad_entries = df0.loc[bad_sel]
-  nbad = len(bad_entries)
-  if (nbad > 0) and (not drop_null):
-    msg = '%d bad runs found in scalar_df' % nbad
-    msg += ', set drop_null to drop bad data.'
-    raise RuntimeError(msg)
-  if (drop_null):
-    df0.drop(bad_entries.index, inplace=True)
-
-  def sq_avg(trace):  # not a well-written function ...
-    """ average error """
-    return np.sqrt(np.sum(trace**2.))/len(trace)
-  from qharv.reel import mole
-  meta = df0['fdat'].apply(mole.interpret_qmcpack_fname).apply(pd.Series)
-  df = pd.concat([meta, df0], axis=1)
-
-  # average over twists
-  cols = df.columns
-  mcol = [col for col in cols if col.endswith('_mean')]
-  ecol = [col for col in cols if col.endswith('_error')]
-  rcol = [col for col in cols if (not col == 'series') and
-          (not col.endswith('_mean')) and (not col.endswith('_error'))]
-  groups = df.groupby('series')
-  mdf = groups[mcol].apply(np.mean)
-  edf = groups[ecol].apply(sq_avg)
-  rdf = groups[rcol].apply(lambda x: x.drop(
-   ['fdat', 'group'], axis=1).drop_duplicates().squeeze())
-
-  df1 = rdf.join(mdf).join(edf).reset_index().sort_values('series')
-  return df1
-
-def taw(ym, ye, weights):
-  """ twist average with weights """
-  wtot = weights.sum()
-  try:
-    aym = np.dot(ym, weights)/wtot
-    aye = np.dot(ye**2, weights**2)**0.5/wtot
-  except ValueError as err:
-    if 'not aligned' not in str(err):
-      raise err
-    aym = (weights[:, np.newaxis]*ym).sum(axis=0)/wtot
-    aye = (weights[:, np.newaxis]**2*ye**2).sum(axis=0)**0.5/wtot
-  return aym, aye
