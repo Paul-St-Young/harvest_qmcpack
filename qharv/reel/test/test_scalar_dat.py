11cf57b113c2da1e6313f3b8e2be966adc3314b1
Paul Young <yyang173@illinois.edu>
2019-11-26 19:04:19 -0600

    update scalar_dat read tests

--- qharv/reel/test/test_scalar_dat.py
+++ qharv/reel/test/test_scalar_dat.py
@@ -1,40 +1,35 @@
 import numpy as np
 
-def test_no_header_parse():
+def test_no_header_read():
   # write test data to file
   fname = 'tmp.dat'
   fdat  = "1 2 3\n4 5 6\n7 8 9"
-  with open(fname,'w') as f:
+  with open(fname, 'w') as f:
     f.write(fdat)
 
   # parse ascii file
   from qharv.reel import scalar_dat
-  df = scalar_dat.parse(fname)
+  df = scalar_dat.read(fname)
 
   # compare parse data with reference
-  ref   = np.arange(9).reshape(3,3) + 1
-  assert np.allclose(df.values,ref)
-# end def test_no_header_parse
+  ref   = np.arange(9).reshape(3, 3) + 1
+  assert np.allclose(df.values, ref)
 
-
-def test_header_parse():
+def test_header_read():
   # write test data to file
   fname = 'tmp.dat'
   fdat  = "# x y z\n1 2 3\n4 5 6\n7 8 9"
-  with open(fname,'w') as f:
+  with open(fname, 'w') as f:
     f.write(fdat)
 
   # parse ascii file
   from qharv.reel import scalar_dat
-  df   = scalar_dat.parse(fname)
+  df   = scalar_dat.read(fname)
   cols = df.columns.values
-  expected_cols = ['x','y','z']
+  expected_cols = ['x', 'y', 'z']
   for icol in range(3):
     assert cols[icol] == expected_cols[icol]
 
   # compare parse data with reference
-  ref   = np.arange(9).reshape(3,3) + 1
-  assert np.allclose(df.values,ref)
-# end def test_header_parse
-  
-
+  ref   = np.arange(9).reshape(3, 3) + 1
+  assert np.allclose(df.values, ref)
