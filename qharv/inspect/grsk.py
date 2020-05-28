1309744fe2515507a158d5f2ea114a38c262ce52
Paul Young <yyang173@illinois.edu>
2020-05-28 15:14:22 -0500

    ase_gofr

--- qharv/inspect/grsk.py
+++ qharv/inspect/grsk.py
@@ -14,3 +14,59 @@ def gr2sk(myk, myr, grm, rho):
 def sk2gr(myr, myk, skm, rho):
   grm = 1+ft_iso3d(myr, myk, skm-1)/rho/(2*np.pi)**3
   return grm
+
+def get_bin_edges(axes, rmin=0., rmax=None, nr=32):
+  # create linear grid
+  if rmax is None:
+    from qharv.inspect import axes_pos
+    rmax = axes_pos.rwsc(axes)/2.
+  bin_edges = np.linspace(rmin, rmax, nr)
+  return bin_edges
+
+def get_gofr_norm(axes, bin_edges, n1, n2=None):
+  from qharv.inspect import axes_pos
+  ndim, ndim = axes.shape
+  assert ndim == 3  # assume 3 dimensions
+  # calculate volume of bins
+  vnorm = np.diff(4*np.pi/3*bin_edges**ndim)
+  # calculate density normalization
+  if n2 is None:
+    npair = n1*(n1-1)/2
+  else:
+    npair = n1*n2
+  volume = axes_pos.volume(axes)
+  rho = npair/volume
+  # assemble the norm vector
+  gr_norm = 1./(rho*vnorm)
+  return gr_norm
+
+def ase_gofr(atoms, bin_edges, gr_norm):
+  from ase.geometry import get_distances
+  ias = np.unique(atoms.get_atomic_numbers())
+  gr1_map = {}  # snapshot g(r) between all pairs of particle types
+  for i in range(len(ias)):
+    for j in range(i, len(ias)):
+      ia = ias[i]
+      ja = ias[j]
+      # select positions
+      idx1 = [atom.index for atom in atoms if atom.number == ia]
+      idx2 = [atom.index for atom in atoms if atom.number == ja]
+      ni = len(idx1)
+      nj = len(idx2)
+
+      # calculate distances
+      drij, rij = get_distances(
+        atoms[idx1].get_positions(),
+        p2=atoms[idx2].get_positions(),
+        cell=atoms.get_cell(),
+        pbc=1
+      )
+      offset = 0
+      if ia == ja:
+        offset = 1
+      idx = np.triu_indices_from(rij, offset)
+      dists = rij[idx]
+      hist, be = np.histogram(dists, bin_edges)
+      gr1 = hist*gr_norm
+      gr1_map[(ia, ja)] = gr1
+  return gr1_map
