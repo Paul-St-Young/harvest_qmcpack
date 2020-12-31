306a353024cd76f3da06a1e07a0b0b05ea5fee6d
Yubo "Paul" Yang <yubo.paul.yang@gmail.com>
2020-12-31 10:37:15 -0600

    parse_vector_print

--- qharv/cross/dirac.py
+++ qharv/cross/dirac.py
@@ -1,5 +1,21 @@
 import pandas as pd
 
+def read(fout, vp_kwargs=None):
+  from qharv.reel import ascii_out
+  if vp_kwargs is None:
+    vp_kwargs = dict()
+  mm = ascii_out.read(fout)
+  etot = ascii_out.name_sep_val(mm, 'Total energy', ':')
+  ehomo = ascii_out.name_sep_val(mm, 'E(HOMO)', ':')
+  elumo = ascii_out.name_sep_val(mm, 'E(LUMO)', ':')
+  data = {
+    'etot': etot,
+    'ehomo': ehomo,
+    'elumo': elumo,
+    'vectors': parse_vector_print(mm, **vp_kwargs)
+  }
+  return data
+
 def parse_ev_text(text):
   lines = text.split('\n')
   entryl = []
@@ -11,9 +27,11 @@ def parse_ev_text(text):
     ibas = int(toks[0])
     elem = toks[2]
     symm = toks[4]
+    # (a, b, c, d) -> a+ib, c+id
     cup = float(toks[-4])+1j*float(toks[-3])
     cdn = float(toks[-2])+1j*float(toks[-1])
-    entry = {'elem': elem, 'ibas': ibas, 'symm': symm,
+    # Kramer's pair: (-c, d, a, -b)
+    entry = {'elem': elem, 'ibas': ibas, 'ao_symm': symm,
              'cup': cup, 'cdn': cdn}
     entryl.append(entry)
   df = pd.DataFrame(entryl)
@@ -40,16 +58,14 @@ def parse_eigenvectors(mm, idxl):
   header = '===================================================='
   trailer = 'Electronic eigenvalue no'
   dfl = []
-  myev = 0
   for i in idxl:
     mm.seek(i)
     line = mm.readline().decode()
     # eg. 'eigenvalue no.  2: -0.2364785578899'
     left, right = line.split(':')
     iev = int(left.split()[-1])
-    myev += 1
     ev = float(right)
-    meta = {'iev': iev, 'ev': ev, 'myev': myev}
+    meta = {'iev': iev, 'ev': ev}
     # read body
     i0, i1 = ascii_out.locate_block(mm, header, trailer,
       force_tail=True, skip_trailer=True)
@@ -62,4 +78,30 @@ def parse_eigenvectors(mm, idxl):
       df1[key] = val
     dfl.append(df1)
   df = pd.concat(dfl, axis=0).reset_index(drop=True)
+  return df
+
+def parse_vector_print(mm,
+  header='* Vector print *',
+  mid_tag='Fermion ircop E1u',
+  end_tag='* Mulliken population analysis *',
+):
+  from qharv.reel import ascii_out
+  # seek to header
+  idx = mm.find(header.encode())
+  mm.seek(idx)
+  # find all potential vectors to read
+  idxl = ascii_out.all_lines_with_tag(mm, 'Electronic eigenvalue no.')
+  # exclude population analysis
+  iend = mm.find(end_tag.encode())
+  if iend > 0:
+    idxl = [i for i in idxl if i < iend]
+  # partition into even and odd
+  imid = mm.find(mid_tag.encode())
+  idxg = [i for i in idxl if i < imid]
+  idxu = [i for i in idxl if i >= imid]
+  gdf = parse_eigenvectors(mm, idxg)
+  gdf['mo_symm'] = 'E1g'
+  udf = parse_eigenvectors(mm, idxu)
+  udf['mo_symm'] = 'E1u'
+  df = pd.concat([gdf, udf], sort=False).reset_index(drop=True)
   return df
