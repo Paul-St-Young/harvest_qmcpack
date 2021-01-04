ac5993812fe437e4366cca300721f186f156921b
Yubo "Paul" Yang <yubo.paul.yang@gmail.com>
2021-01-04 17:13:20 -0600

    parse_mulliken

--- qharv/cross/dirac.py
+++ qharv/cross/dirac.py
@@ -38,7 +38,7 @@ def parse_ev_text(text):
   return df
 
 def parse_eigenvectors(mm, idxl):
-  """Parse eigenvectors from DIRAC 'Vector print' output
+  """Parse eigenvectors from DIRAC 'Vector print' .PRIVEC output
 
   Args:
     mm (mmap.mmap): memory map of outputfile
@@ -102,6 +102,79 @@ def parse_vector_print(mm,
   gdf = parse_eigenvectors(mm, idxg)
   gdf['mo_symm'] = 'E1g'
   udf = parse_eigenvectors(mm, idxu)
+  udf['mo_symm'] = 'E1u'
+  df = pd.concat([gdf, udf], sort=False).reset_index(drop=True)
+  return df
+
+def parse_populations(mm, idxl):
+  """Parse population from DIRAC 'Mulliken' .MULPOP output
+   modified from parse_eigenvectors
+
+  Args:
+    mm (mmap.mmap): memory map of outputfile
+    idxl (list): a list of starting memory locations for eigenvectors
+  Return:
+    pd.DataFrame: population information
+  """
+  from qharv.reel import ascii_out
+  header = '--------------------------------------'
+  trailer = 'Electronic eigenvalue no'
+  entryl = []
+  for i in idxl:
+    mm.seek(i)
+    line = mm.readline().decode()
+    # eg. 'eigenvalue no.  2: -0.2364785578899  ('
+    toks = line.split(':')
+    left = toks[0]
+    right = toks[1].split()[0]
+    iev = int(left.split()[-1])
+    ev = float(right)
+    meta = {'iev': iev, 'ev': ev}
+    # read body
+    i0, i1 = ascii_out.locate_block(mm, header, trailer,
+      force_tail=True, skip_trailer=True)
+    if i1 < 0:
+      i0, i1 = ascii_out.locate_block(mm, header, '**')
+    # parse
+    text = mm[i0:i1].decode()
+    # e.g.
+    # alpha    1.0000  |      1.0000
+    # beta     0.0000  |      0.0000
+    lines = text.split('\n')
+    aline = lines[0]
+    bline = lines[1]
+    assert 'alpha' in aline
+    assert 'beta' in bline
+    atot = float(aline.split()[1])
+    btot = float(bline.split()[1])
+    entry = {'a_tot': atot, 'b_tot': btot}
+    entry.update(meta)
+    entryl.append(entry)
+  df = pd.DataFrame(entryl)
+  return df
+
+def parse_mulliken(mm,
+  header='* Mulliken population analysis *',
+  mid_tag='Fermion ircop E1u',
+  end_tag='** Total gross population **',
+):
+  from qharv.reel import ascii_out
+  # seek to header
+  idx = mm.find(header.encode())
+  mm.seek(idx)
+  # find all potential vectors to read
+  idxl = ascii_out.all_lines_with_tag(mm, 'Electronic eigenvalue no.')
+  # exclude extra lines
+  iend = mm.find(end_tag.encode())
+  if iend > 0:
+    idxl = [i for i in idxl if i < iend]
+  # partition into even and odd
+  imid = mm.find(mid_tag.encode())
+  idxg = [i for i in idxl if i < imid]
+  idxu = [i for i in idxl if i >= imid]
+  gdf = parse_populations(mm, idxg)
+  gdf['mo_symm'] = 'E1g'
+  udf = parse_populations(mm, idxu)
   udf['mo_symm'] = 'E1u'
   df = pd.concat([gdf, udf], sort=False).reset_index(drop=True)
   return df
