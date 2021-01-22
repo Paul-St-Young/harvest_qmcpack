d654ef0b9affa4ec9a0d09677f2cac62ce20d60b
Yubo "Paul" Yang <yubo.paul.yang@gmail.com>
2021-01-22 13:16:02 -0600

    allow multiple observables in scalar_h5

--- qharv/sieve/scalar_h5.py
+++ qharv/sieve/scalar_h5.py
@@ -7,49 +7,61 @@ def extract_twists(fh5, **suffix_kwargs):
 
   each twist should be a group in at root
 
-  example:
+  example fh5:
     twist000
       myr
       gr_mean
       gr_error
+      sk_mean
+      sk_error
     twist001
       myr
       gr_mean
       gr_error
+      sk_mean
+      sk_error
+  return:
+    {'myr': [], 'gr_mean': [], 'gr_error': [], 'sk_mean': [], 'sk_error': []}
 
   Args:
     fh5 (str): h5 file location
   Return:
-    (dict, np.array, np.array): (meta data, mean, error)
+    dict: (meta, mean, error)
   """
+  from qharv.sieve.mean_df import categorize_columns
   fp = h5py.File(fh5, 'r')
-  # determine ymean, yerror from first twist
+  # determine meta, ymean, yerror from first twist
   twist0 = list(fp.keys())[0]
-  ymean, yerror = get_ymean_yerror(fp, twist0, **suffix_kwargs)
+  cols = fp[twist0].keys()
+  labels, mcols, ecols = categorize_columns(cols)
   # treat all other entries as metadata
   meta = {}
-  for name in fp[twist0].keys():
-    if name not in [ymean, yerror]:
-      meta[name] = fp[os.path.join(twist0, name)][()]
+  for name in labels:
+    meta[name] = fp[os.path.join(twist0, name)][()]
   # extract all ymean and yerror
-  yml = []
-  yel = []
-  twists = fp.keys()
-  # !!!! make sure twists are sorted
-  itwists = [int(t.replace('twist', '')) for t in twists]
-  assert sorted(itwists)
-  ntwist = len(twists)
-  for twist in twists:
-    mpath = os.path.join(twist, ymean)
-    ym1 = fp[mpath][()]
-    epath = os.path.join(twist, yerror)
-    ye1 = fp[epath][()]
-    yml.append(ym1)
-    yel.append(ye1)
+  data = {}
+  for ymean, yerror in zip(mcols, ecols):
+    yml = []
+    yel = []
+    twists = fp.keys()
+    # !!!! make sure twists are sorted
+    itwists = [int(t.replace('twist', '')) for t in twists]
+    assert sorted(itwists)
+    ntwist = len(twists)
+    for twist in twists:
+      mpath = os.path.join(twist, ymean)
+      ym1 = fp[mpath][()]
+      epath = os.path.join(twist, yerror)
+      ye1 = fp[epath][()]
+      yml.append(ym1)
+      yel.append(ye1)
+    yma = np.array(yml)  # mean
+    yea = np.array(yel)  # error
+    data[ymean] = yma
+    data[yerror] = yea
   fp.close()
-  yma = np.array(yml)  # mean
-  yea = np.array(yel)  # error
-  return meta, yma, yea
+  data.update(meta)
+  return data
 
 def twist_average_h5(fh5, weights=None, **suffix_kwargs):
   """ twist average data in an HDF5 archive
@@ -61,35 +73,24 @@ def twist_average_h5(fh5, weights=None, **suffix_kwargs):
   Return:
     (dict, np.array, np.array): (meta data, mean, error)
   """
-  from qharv.sieve.mean_df import taw
-  meta, yma, yea = extract_twists(fh5, **suffix_kwargs)
-  ntwist = len(yma)
+  from qharv.sieve.mean_df import taw, categorize_columns
+  data = extract_twists(fh5, **suffix_kwargs)
+  labels, mcols, ecols = categorize_columns(data.keys())
+  ntwist = len(data[mcols[0]])
   # twist average with weights
   if weights is None:
     weights = np.ones(ntwist)
   else:
     if len(weights) != ntwist:
       raise RuntimeError('%d weights for %d twists' % (len(weights), ntwist))
-  ym, ye = taw(yma, yea, weights)
-  return meta, ym, ye
-
-def get_ymean_yerror(fp, twist0, msuffix='_mean', esuffix='_error'):
-  ymean = None
-  yerror = None
-  for name in fp[twist0].keys():
-    if name.endswith(msuffix):
-      ymean = name
-    if name.endswith(esuffix):
-      yerror = name
-  if ymean is None:
-    raise RuntimeError('no entry with suffix %s' % msuffix)
-  if yerror is None:
-    raise RuntimeError('no entry with suffix %s' % esuffix)
-  ynamem = '_'.join(ymean.split('_')[:-1])
-  ynamee = '_'.join(yerror.split('_')[:-1])
-  if ynamem != ynamee:
-    raise RuntimeError('yname mismatch')
-  return ymean, yerror
+  # twist average mean, error columns
+  for ymean, yerror in zip(mcols, ecols):
+    yma = data[ymean]
+    yea = data[yerror]
+    ym, ye = taw(yma, yea, weights)
+    data[ymean] = ym
+    data[yerror] = ye
+  return data
 
 def twist_concat_h5(fh5, name, twists=None):
   fp = h5py.File(fh5, 'r')
