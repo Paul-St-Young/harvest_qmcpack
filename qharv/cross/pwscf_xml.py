525251287d180dda11173710aeb08d9cf3c916ae
Yubo "Paul" Yang <pyang@flatironinstitute.org>
2021-12-02 12:38:22 -0500

    double occupation for RKS

--- qharv/cross/pwscf_xml.py
+++ qharv/cross/pwscf_xml.py
@@ -62,11 +62,17 @@ def read_bands(doc):
 
 def read_occupations(doc):
   bs = doc.find('.//band_structure')
+  lsda = read_true_false(bs, 'lsda')
+  noncolin = read_true_false(bs, 'noncolin')
+  if lsda or noncolin:
+    ymult = 1
+  else:  # RKS
+    ymult = 2
   ocl = bs.findall('.//occupations')
   ol = []
   for oc in ocl:
     o1 = text2arr(oc.text, flatten=True)
-    ol.append(o1)
+    ol.append(ymult*o1)
   omat = np.array(ol)
   return omat
 
