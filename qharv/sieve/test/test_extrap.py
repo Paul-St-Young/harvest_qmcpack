b8092d847f42cafa63691af526d5f92772c66ee0
Yubo "Paul" Yang <pyang@flatironinstitute.org>
2022-05-06 16:40:11 -0400

    move polyextrap

--- qharv/sieve/test/test_extrap.py
+++ qharv/sieve/test/test_extrap.py
@@ -1,5 +1,5 @@
 import numpy as np
-from qharv.sieve import mean_df
+from qharv.sieve.extrap import polyextrap
 
 def data_with_noise(norder=1):
   iseed = 42
@@ -17,15 +17,15 @@ def data_with_noise(norder=1):
 def test_linear_no_error():
   x = np.array([1, 2, 3])
   y = np.array([1, 2, 3])
-  y0 = mean_df.polyextrap(x, y)
+  y0 = polyextrap(x, y)
   assert np.isclose(y0, 0)
-  y1 = mean_df.polyextrap(x, y, xtarget=1)
+  y1 = polyextrap(x, y, xtarget=1)
   assert np.isclose(y1, 1)
 
 def test_linear_with_error():
   nsig = 3
   x, ym, ye = data_with_noise()
-  y0m, y0e = mean_df.polyextrap(x, ym, ye)
+  y0m, y0e = polyextrap(x, ym, ye)
   assert (y0m-nsig*y0e < 0) & (0 < y0m+nsig*y0e)
   assert np.isclose(y0m, 0.0001844)
   assert np.isclose(y0e, 0.00152759)
@@ -33,7 +33,7 @@ def test_linear_with_error():
 def test_quadratic_with_error():
   nsig = 3
   x, ym, ye = data_with_noise(norder=2)
-  y0m, y0e = mean_df.polyextrap(x, ym, ye, 2)
+  y0m, y0e = polyextrap(x, ym, ye, 2)
   assert (y0m-nsig*y0e < 0) & (0 < y0m+nsig*y0e)
   assert np.isclose(y0m, 0.0025526239)
   assert np.isclose(y0e, 0.00435896876)
