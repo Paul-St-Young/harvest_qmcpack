c7ec341a90c90906b9df550a031025fe0549e344
Yubo "Paul" Yang <pyang@flatironinstitute.org>
2022-05-06 18:08:29 -0400

    mix and ts extrap

--- qharv/sieve/extrap.py
+++ qharv/sieve/extrap.py
@@ -38,3 +38,59 @@ def polyextrap(x, ym, ye=None, order=1, xtarget=0, return_fit=False):
   if return_fit:
     ret = (y0, popt, perr)
   return ret
+
+def mix_extrap(df, yname):
+  assert df.index.is_unique
+  # extract VMC value
+  sel0 = df.method == 'vmc'
+  assert len(df.loc[sel0]) == 1
+  ymn = '%s_mean' % yname
+  yen = '%s_error' % yname
+  y0m = df.loc[sel0, ymn].values.squeeze()
+  y0e = df.loc[sel0, yen].values.squeeze()
+  # calculate 2*DMC-VMC
+  sel1 = df.method == 'dmc'
+  dm = 2*df.loc[sel1, ymn]-y0m
+  de = ((4*df.loc[sel1, yen])**2+y0e**2)**0.5
+  df1 = pd.concat([dm, de], axis=1)
+  return df1
+
+def mix_estimator(df, ynames):
+  labels = ['path', 'id']
+  sufs = ['_mean', '_error']
+  for yname in ynames:
+    mydf = df.groupby(labels).apply(
+      mix_extrap, yname).reset_index()
+    # find entries in original data frame
+    idx = mydf['level_2'].values
+    for suf in sufs:
+      col = '%s%s' % (yname, suf)
+      icol = df.columns.get_loc(col)
+      # set entry
+      df.iloc[idx, icol] = mydf[col].values
+
+def ts_extrap(df, yname, xname='timestep'):
+  from qharv.sieve import mean_df
+  x, ym, ye = mean_df.xyye(df, xname, yname)
+  y0m, y0e = polyextrap(x, ym, ye)
+  ret = pd.Series({
+    '%s_mean' % yname: y0m,
+    '%s_error' % yname: y0e,
+  })
+  return ret
+
+def timestep(df, labels, ynames, xname='timestep'):
+  sufs = ['_mean', '_error']
+  # keep one entry per group
+  df1 = df.groupby(labels).first()
+  df1.drop(columns=xname, inplace=True)
+  # replace targeted columns with extrapolated data
+  for yname in ynames:
+    # extrapolate
+    mydf = df.groupby(labels).apply(
+      ts_extrap, yname, xname=xname)
+    # replace
+    for suf in sufs:
+      col = '%s%s' % (yname, suf)
+      df1[col] = mydf[col]
+  return df1.reset_index()
